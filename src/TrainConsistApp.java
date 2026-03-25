0be4249675fec1ef50458fa4aa0ab9bce32a6155
prahlad-ctrl <alaparthi.prahlad@gmail.com>
2026-03-25 14:10:09 +0530

    UC5

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -4,19 +4,14 @@ class TrainConsistApp {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
 
-        LinkedList<String> consist = new LinkedList<>();
+        LinkedHashSet<String> formation = new LinkedHashSet<>();
 
-        consist.add("Engine");
-        consist.add("Sleeper");
-        consist.add("AC");
-        consist.add("Cargo");
-        consist.add("Guard");
+        formation.add("Engine");
+        formation.add("Sleeper");
+        formation.add("Cargo");
+        formation.add("Guard");
+        formation.add("Sleeper");
 
-        consist.add(2, "Pantry Car");
-
-        consist.removeFirst();
-        consist.removeLast();
-
-        System.out.println("Final Train Consist: " + consist);
+        System.out.println("Train Formation: " + formation);
     }
 }
