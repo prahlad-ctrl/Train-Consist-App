92916773c68e6a68c82b1138b47c62b149b710fa
prahlad-ctrl <alaparthi.prahlad@gmail.com>
2026-04-10 17:00:56 +0530

    UC19

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -6,14 +6,23 @@ class TrainConsistApp {
 
         String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
 
-        String searchKey = "BG309";
+        String key = "BG309";
 
+        int low = 0, high = bogieIds.length - 1;
         boolean found = false;
 
-        for (String id : bogieIds) {
-            if (id.equals(searchKey)) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            int cmp = key.compareTo(bogieIds[mid]);
+
+            if (cmp == 0) {
                 found = true;
                 break;
+            } else if (cmp > 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
 
